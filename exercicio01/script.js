dbee7cd96d8e5d6cad0335da00ed64c756929f3c
Dayanne Alcantara <dayannealcantara14@gmail.com>
2021-12-09 15:52:12 -0300

    código JS

---
+++ exercicio01/script.js
@@ -0,0 +1,19 @@
+// alert ('olá')
+function carregar() {
+  var mensagem = document.getElementById("msg");
+  var imagem = document.getElementById("img");
+  var data = new Date();
+   var hora = data.getHours()
+//   var hora = 19;
+  mensagem.innerHTML = `Agora são ${hora}h`;
+  if (hora >= 0 && hora < 12) {
+    imagem.src = 'dia.jpeg';
+    document.body.style.background = '#e2cd9f'
+  } else if (hora >= 12 && hora < 18) {
+    imagem.src = "tarde.jpg";
+    document.body.style.background = '#b9846f'
+  } else {
+    imagem.src = "noite.jpg";
+    document.body.style.background = '#515154'
+  }
+}
